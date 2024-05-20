6e0d02c1a67b56fc3981264a70b88616a472b8f5
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-05-20 22:09:53 +0300

    main

--- hangmanGame/main.js
+++ hangmanGame/main.js
@@ -1,12 +1,40 @@
+const wordDisplay = document.querySelector(".word-display");
 const keyboardDiv = document.querySelector(".keyboard");
 
+
+let currentWord, wrongGuessCount = 0;
+
 const getRandomWord = () => {
-    
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    const { word, hint } = wordList[randomIndex];
+    console.log(word);
+    currentWord = word;
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map( () => `<li class="letter"></li>`).join("");
+
+}
+
+const initGame = (button, clickedLetter) => {
+    // console.log(button, clickedLetter);
+    if (currentWord.includes(clickedLetter)) {
+        [...currentWord].forEach((letter, index) => {
+            if (letter === clickedLetter) {
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
+    } else {
+        console.log(clickedLetter, "is not exist on the word");
+    }
+
 }
 
 for (let i = 97; i <= 122; i++) {
-    const button = createElement("button");
+    const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
     
 }
+
+getRandomWord();
